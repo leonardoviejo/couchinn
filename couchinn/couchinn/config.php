2beb95dbc8a1627de40f432f43123bde913c6d57
M89g <miguel.macagno@gmail.com>
2016-06-06 21:25:56 -0300

    Archivos innecesarios borrados
    No seran utilizados

--- couchinn/couchinn/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$conexion = mysqli_connect('localhost','root','','couchinn');
-	if (mysqli_connect_errno()) {
-		echo "Falló al conectar con la base de datos: " . mysqli_connect_error();
-	}
-?>
